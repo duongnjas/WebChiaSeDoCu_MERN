277a18b0df1b8d10a472b98f10cd8bebf5d480ff
Duong Nguyen <duyduongdg@gmail.com>
2022-11-15 19:09:22 +0700

    first commit

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+
+export default function index() {
+  return (
+    <div style={{backGroundColor:"#ffba00"}}>
+      abc
+      <img src="https://static.chotot.com/storage/default/transparent_logo.webp"/>
+    </div>
+  )
+}
